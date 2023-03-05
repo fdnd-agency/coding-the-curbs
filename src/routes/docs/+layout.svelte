8759bf75cd84b977ebc807902156df23e34fe449
ju5tu5 <justus@ju5tu5.nl>
2023-03-05 20:30:39 +0100

    Changed a little doc thing

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -3,15 +3,15 @@
 
 	const data = {
 		Smartzone: {
-			// GET: '/docs/method#get',
-			// '[slug]': '/docs/method#slug'
+			GET: '/docs/method#get',
+			'[slug]': '/docs/method#slug'
 			// POST: '/docs/method#post',
 			// PUT: '/docs/method#put',
 			// PATCH: '/docs/method#patch',
 			// DELETE: '/docs/method#delete',
 		},
 		Smartzones: {
-			// GET: '/docs/methods#get'
+			GET: '/docs/methods#get'
 		},
 		
 	};
--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,1 +1,12 @@
-<h2>It's a page</h2>
+<h2>Smartzone</h2>
+
+<h2>Smartzones</h2>
+
+<h3>GET</h3>
+
+<h4>Voorbeeld requests:</h4>
+<ul>
+  <li>
+    <a href="/api/v1/smartzones">/api/v1/smartzones</a>
+  </li>
+</ul>
